06a523db9e0d52cfb8437bb54bfa748cea51c72c
Saikat-Adhya <saikatadhya1@gmail.com>
2025-07-05 20:00:13 +0530

    Changes for hosting in the project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/WorldKnowldge_React_Project/', // 👈 Important!
   plugins: [react()],
-})
+});
